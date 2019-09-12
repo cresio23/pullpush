54b6a4d430a21d4b50b128a5ac1335c75c5d8db4
chaseresio <54462708+chaseresio@users.noreply.github.com>
2019-09-12 10:56:33 -0500

    Create pushpull.c

---
+++ pushpull.c
@@ -0,0 +1,19 @@
+/************************************
+
+* Author: Chase Resio
+* Date: 9/11/19
+
+* This program is a simple "Hello
+  World" except it prints out a
+  name that you put in to practice
+  pushing and pulling from GitHub
+
+*************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  printf("Chase Resio\n");
+  return 0;
+}
